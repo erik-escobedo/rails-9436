80eb67d1b3361f5c1e4b6d5d45b3fc64846a14fb
Dmitry Maksimov <dmtmax@gmail.com>
2013-02-26 17:10:13 +0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+Sample application for issue [#9436](https://github.com/rails/rails/issues/9436).
+
+Asset `app/assets/javascripts/hello.jst.ejs.haml` is not processed
+properly.
+
+Asset `app/assets/javascripts/goodbye.jst.ejs.erb` is processed without any error.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'df543808c92b08107681033fe6850d6f8a90b109901bfc70415467d5a9718203078b8ee3fe84089692ec85ee9662b61e75c26bc686ec44f1e21b9a68c1febfec'
